0d5b45a95e9c7f4d5382bd33733f055c4d189f6d
nast0yka <anse.soroka@gmail.com>
2023-05-31 16:16:27 +0300

    week-2
    не работает отрисовка выбранного поста

--- scripts/modalW.js
+++ scripts/modalW.js
@@ -1,14 +1,22 @@
-let body = document.querySelector('body');
-let bodyOverlay = document.querySelector('.body-overlay');
+import { exportFetch, numberOfPost } from './main.js';
 
-let addPhoto = document.querySelector('#add-photo');
-let addPostModal = document.querySelector('.add-post-modal');
-let countPhoto = document.querySelector('#photo-count');
+const body = document.querySelector('body');
+const bodyOverlay = document.querySelector('.body-overlay');
+
+const addPhoto = document.querySelector('#add-photo');
+const addPostModal = document.querySelector('.add-post-modal');
+export const countPhoto = document.querySelector('#photo-count');
 
 function makeOverLay() {
     addPostModal.classList.add('active');
     body.classList.add('with-overlay');
     bodyOverlay.classList.add('active');
+}
+
+function awayOverLay() {
+    addPostModal.classList.remove('active');
+    body.classList.remove('with-overlay');
+    bodyOverlay.classList.remove('active');
 }
 
 export function addPhotoF() {
@@ -20,37 +28,41 @@ export function addPhotoF() {
     };
 }
 
-let addPostStep1 = document.querySelector('.add-post-modal__step-1');
-let addPostStep2 = document.querySelector('.add-post-modal__step-2');
-let modalFooter = document.querySelector('.modal__footer');
-let inputFilePhoto = document.querySelector('#file-upload');
-let uploadedPhoto = document.querySelector('#uploaded-photo');
+const addPostStep1 = document.querySelector('.add-post-modal__step-1');
+const addPostStep2 = document.querySelector('.add-post-modal__step-2');
+const modalFooter = document.querySelector('.modal__footer');
+const inputFilePhoto = document.querySelector('#file-upload');
+const uploadedPhoto = document.querySelector('#uploaded-photo');
+const areaText = document.querySelector('#post-text');
+const areaHashtag = document.querySelector('#post-hashtags');
 
-export function inputFilePhotoF() {
+export function inputFilePhotoExport() {
+
     inputFilePhoto.addEventListener("change", () => {
         addPostStep1.classList.add('hidden');
         addPostStep2.classList.remove('hidden');
         addPostStep2.classList.add('activ');
         modalFooter.classList.remove('hidden');
 
-        //вставка нового фото
-        const selectedFiles = [...inputFilePhoto.files];
-        // for (const f of selectedFiles) {
-        //     uploadedPhoto.src = f;
-        // }
-
-        var typedArray = selectedFiles;
-        var blob = new Blob([typedArray], { type: 'application/octet-binary' });
-        var url = URL.createObjectURL(blob);
-        uploadedPhoto.src = url;
+        //отображение нового фото
+        const file = inputFilePhoto.files[0];
+        uploadedPhoto.src = URL.createObjectURL(file);
 
     });
 }
 
-let publishPost = document.querySelector('#post-publish');
+//функция закрытия модально окна по клику
+export function closeModal() {
+    bodyOverlay.addEventListener("click", (event) => {
+        awayOverLay();
+    });
+}
+
+const publishPost = document.querySelector('#post-publish');
 
 // успешный результат отправки поста
 const resultSendingSuccess = document.querySelector("#alert-success");
+const containerSuccess = document.querySelector('.alert--success');
 
 function prepareResultSuccess() {
     const resultSuccess = resultSendingSuccess.content.cloneNode(true);
@@ -59,7 +71,7 @@ function prepareResultSuccess() {
 
 // неуспешный результат отправки поста
 const resultSendingFail = document.querySelector("#alert-fail");
-let containerFail = resultSendingFail.querySelector('.alert--error');
+const containerFail = document.querySelector('.alert--error');
 
 function prepareResultFail() {
     const resultFail = resultSendingFail.content.cloneNode(true);
@@ -67,55 +79,128 @@ function prepareResultFail() {
     return resultFail;
 }
 
-// функция исчезания уведомления
-function addHiddenAlert(result) {
-    result.classList.add('hidden');
-}
-
 
 export function publishPostE() {
-    publishPost.addEventListener("submit", (event) => {
+    publishPost.addEventListener("click", (event) => {
         event.preventDefault();
-
         let sendText = areaText.value;
         let sendHashtag = areaHashtag.value;
-
-        let formData = new FormData();
-        formData.append(sendText, sendHashtag, inputFilePhoto);
+        const formData = new FormData();
+        formData.append('image', inputFilePhoto.files[0]);
+        formData.append('text', sendText);
+        formData.append('tags', sendHashtag);
 
         fetch('https://c-gallery.polinashneider.space/api/v1/posts/', {
                 method: "POST",
                 headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Bearer: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjg5MzEwNDE1LCJpYXQiOjE2ODQ0NzIwMTUsImp0aSI6ImZiNzczMDY4Mjk5YjQ1YWZhZjgyMGI1YzA0NTkyMGFiIiwidXNlcl9pZCI6NDB9.49_6gGk7w-r9a9o_4A7L1HI2CGhjztGZadKw2JIKh6k'
+                    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjg5MzEwNDE1LCJpYXQiOjE2ODQ0NzIwMTUsImp0aSI6ImZiNzczMDY4Mjk5YjQ1YWZhZjgyMGI1YzA0NTkyMGFiIiwidXNlcl9pZCI6NDB9.49_6gGk7w-r9a9o_4A7L1HI2CGhjztGZadKw2JIKh6k'
                 },
-                body: JSON.stringify(formData)
+                body: formData
             })
             .then((result) => {
-                setTimeout(addHiddenAlert(resultSendingSuccess), 2000);
-                bodyOverlay.append(prepareResultSuccess());
+                body.append(prepareResultSuccess());
+
+                setTimeout(function removeElement() {
+                    let removable = document.querySelector('.alert--success');
+                    removable.remove();
+                }, 2000);
+
                 return result.json();
             })
             .then((data) => {
                 console.log(data);
             })
             .catch((error) => {
-                formData.reset();
-                bodyOverlay.append(prepareResultFail());
-                setTimeout(addHiddenAlert(resultSendingFail), 2000);
+                body.append(prepareResultFail());
                 console.log(error);
+
+                setTimeout(function removeElement() {
+                    let removable = document.querySelector('.alert--error');
+                    removable.remove();
+                }, 2000);
             })
             .finally(() => {
-                formData.reset();
+                areaText.textContent = "";
+                areaHashtag.textContent = "";
+                uploadedPhoto.src = '';
+                awayOverLay();
+                addPostStep1.classList.remove('hidden');
+                addPostStep2.classList.add('hidden');
+                addPostStep2.classList.remove('activ');
+                modalFooter.classList.add('hidden');
+
             })
     });
 }
 
-export function closeModal() {
-    bodyOverlay.addEventListener("click", (event) => {
-        addPostModal.classList.remove('active');
-        bodyOverlay.classList.remove('active');
-        body.classList.remove('with-overlay');
+//открытие превью поста
+
+const modalPreview = document.querySelector(".preview-post-modal");
+const previewAllContent = document.querySelector('.photos__content');
+const modalContent = document.querySelector('.modal__content');
+
+function getDataForViewPost() {
+    exportFetch();
+};
+
+export function previewOfPostE() {
+    previewAllContent.addEventListener("click", (event) => {
+        event.preventDefault();
+        modalPreview.classList.toggle('active');
+        let target = event.target;
+        let numberChoosedPost = target.closest('numberOfPost');
+
+        getDataForViewPost()
+            .then((result) => {
+                return result.json();
+            })
+            .then((data) => {
+                console.log(data);
+                data.forEach((item) => {
+                    const { id, text, image, tags, comments } = item;
+
+                    if (id === numberChoosedPost) { //не знаю , что писать в сравнение
+                        let modalImg = modalContent.querySelector('img');
+                        modalImg.src = image;
+
+                        let modalText = modalContent.querySelector('.post-text');
+                        modalText.textContent = text;
+
+                        tags.forEach((item) => {
+                            let modalHasgtags = modalContent.querySelector('.post-hashtags');
+                            const tagOfModalPost = document.createElement("a");
+                            tagOfModalPost.textContent = item;
+                            modalHasgtags.append(tagOfModalPost);
+                        });
+
+                    } else {}
+                });
+            })
     });
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const textOfPost = document.createElement("p");
+// textOfPost.textContent = text;
+// containerPost.append(textOfPost);
+
+// const tagsOfPost = document.createElement("span");
+// tagsOfPost.textContent = tags;
+// containerPost.append(tagsOfPost);
+
+// const comentsOfPost = document.createElement("div"); 
+// comentsOfPost.textContent = comments;
+// containerPost.append(comentsOfPost);
